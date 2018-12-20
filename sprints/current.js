39ef1f2565a680e64a65a3f7937281778c82fc51
Thuan Ho <hot@mti-tech.vn>
2018-12-20 09:57:51 +0700

    edit

--- sprints/current.js
+++ sprints/current.js
@@ -1,15 +1,31 @@
-var labels = [
-    "20-Dec",
-    "21-Dec",
-    "24-Dec",
-    "25-Dec",
-    "26-Dec",
-    "27-Dec",
-    "28-Dec",
-    "2-Jan",
-    "3-Jan",
-];
+var labels = {
+    's17': [
+        "6-Dec",
+        "7-Dec",
+        "10-Dec",
+        "11-Dec",
+        "12-Dec",
+        "13-Dec",
+        "14-Dec",
+        "17-Dec",
+        "18-Dec",
+    ],
+    's18': [
+        "20-Dec",
+        "21-Dec",
+        "24-Dec",
+        "25-Dec",
+        "26-Dec",
+        "27-Dec",
+        "28-Dec",
+        "2-Jan",
+        "3-Jan",
+    ]
+};
 
-var sprintData = [];
+var sprintData = {
+    's17': [176, 148, 135, 117, 95, 80, 55, 28, 0],
+    's18': []
+};
 
-var maxHours = 176.5;
+var maxHours = { 's17': 176, 's18': 176.5 };
