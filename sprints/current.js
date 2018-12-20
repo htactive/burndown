f514a944f23accda02b290fb74a025db9433cd46
Thuan Ho <hot@mti-tech.vn>
2018-12-20 09:16:06 +0700

    init

---
+++ sprints/current.js
@@ -0,0 +1,15 @@
+var labels = [
+    "20-Dec",
+    "21-Dec",
+    "24-Dec",
+    "25-Dec",
+    "26-Dec",
+    "27-Dec",
+    "28-Dec",
+    "2-Jan",
+    "3-Jan",
+];
+
+var sprintData = [];
+
+var maxHours = 176.5;
